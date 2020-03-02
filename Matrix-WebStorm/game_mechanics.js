d37cbc25fe08ec6ad02d944a90c30427e039bd91
pavTiger <pavTiger@gmail.com>
2020-03-03 00:23:58 +0300

    Textures controls

---
+++ Matrix-WebStorm/game_mechanics.js
@@ -0,0 +1,9 @@
+class Bullet {
+    constructor(start_x, start_y) {
+        this.height = 1;
+        this.width = 1;
+        this.x = start_x;
+        this.y = start_y;
+        this.rotation
+    }
+}
