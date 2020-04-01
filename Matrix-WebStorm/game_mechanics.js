e0f0208d4c6b3a36035aa1974a385bbc57925ae5
dprozz <dmitri.proskuriakov@gmail.com>
2020-04-01 09:40:52 +0300

    Bullets added

--- Matrix-WebStorm/game_mechanics.js
+++ Matrix-WebStorm/game_mechanics.js
@@ -1,12 +1,23 @@
 class Bullet {
-    constructor(start_x, start_y, start_z, dx, dy, dz, bullet_damage, effect) {
+    constructor(start_x, start_y, start_z, dx, dy, dz, damage, effect, shape) {
         this.x = start_x;
         this.y = start_y;
         this.z = start_z;
         this.dx = dx;
         this.dy = dy;
         this.dz = dz;
-        this.damage = bullet_damage;
+        this.damage = damage;
         this.effect = effect;
+        this.body = shape;
+        console.log(this.body)
+        this.body.position.set(this.x, this.y, this.z);
+        console.log(this.body)
+    }
+    update(){
+        this.x += this.dx;
+        this.y += this.dy;
+        this.z += this.dz;
+        this.body.position.set(this.x, this.y, this.z);
+        console.log(1,this.body)
     }
 }
